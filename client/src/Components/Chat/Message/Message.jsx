f2771cd216c84ec62690cafa11e39c29ad1fc65b
Mudit Bisht <3145159Noobi7uke@users.noreply.github.com>
2020-07-19 18:42:22 +0530

    Added read-more and contract properties to Message component

--- client/src/Components/Chat/Message/Message.jsx
+++ client/src/Components/Chat/Message/Message.jsx
@@ -1,27 +1,49 @@
-import React from "react";
-import "./style.css"
+import React,{useState} from "react";
 import ReactEmoji from "react-emoji";
+import "./style.css"
 
-const Message = ({user, name}) => {
-
+const Message = ({user:msg, name}) => {
+    const [countLen, setCountLen] = useState(1);
+    const maxCount = Math.ceil(msg.text.length / 200);
 
     const currentUser = name.trim().toLowerCase();
 
-    // console.log("name = ", name, "user.user.name", user.user);
+    function getMessage(){
+        return msg.text.substr(0, countLen * 200);
+    }
+
+    function extendMessage() {
+        setCountLen(countLen + 1);
+    }
+    function contractMessage() {
+        setCountLen(1);
+    }
+
 
     return (
-        currentUser === user.user ? (
+        currentUser === msg.user ? (
             <div className="chatBox justify-end">
                 <div className="text">
-                <div className="usertext user-color" >{ReactEmoji.emojify(user.text)}</div> 
+                    <div className="usertext user-color" >
+                        {ReactEmoji.emojify(getMessage())}
+                        {countLen < maxCount && <span onClick={extendMessage} className="read-more">  read more..</span>}
+                        <br/>
+                        {countLen>1 && <span onClick={contractMessage}className="read-more"> contract</span>}
+                    </div> 
                     <div className="username">you</div> 
                 </div>
             </div>
         ) : (
                 <div className="chatBox justify-start ">
                     <div className="text">
-                    <div className="username">{user.user}</div> 
-                    <div className="usertext">{ReactEmoji.emojify(user.text)}</div> 
+                        <div className="username">{msg.user}</div> 
+                        <div className="usertext">
+                            {ReactEmoji.emojify(getMessage())}
+                            {countLen < maxCount && <span onClick={extendMessage} className="read-more">  read more..</span>}
+                            <br/>
+                            {countLen>1 && <span onClick={contractMessage}className="read-more"> contract</span>}
+                        </div> 
+                        
                     </div>
                 </div>
             )
